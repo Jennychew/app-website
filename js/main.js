ba3f4aecfd84d31a77bc9b20f9b70fc04ae79318
Jennychew <jennychew@jennys-MacBook-Pro.local>
2014-01-14 10:42:18 -0500

    font size changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
-var $button = $('.button');
+var $items = $('.items img');
 
-$('#buy-now').on("click", function () {
-   $('.button').css('background', 'red');
- });
+var switchItem = function (current, incoming) {
+	$items.eq(incoming).attr('data-state', 'incoming').fadeIn(250, function(){
+		$items.eq(current).hide().attr('data-state', '')
+		$items.eq(incoming).attr('data-state', 'current')	
+	
+	});
+};
+
+$items.filter(':not([data-state="current"])').hide();
+
+$('.next').on('click', function (){
+	var current = $items.filter('[data-state="current"]').index();
+	var next = current + 1;
+	
+	if (next > $items.length - 1) {
+	next = 0;
+	}
+	
+	switchItem(current, next);
+});
+
+$items.filter(':not([data-state="current"])').hide();
+
+$('.previous').on('click', function (){
+	var current = $items.filter('[data-state="current"]').index();
+	var next = current - 1;
+	
+	if (next > $items.length + 1) {
+	next = 0;
+	}
+	
+	switchItem(current, next);
+});
