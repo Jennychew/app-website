6563d754c3d0411e0fc3f68c5b0a47094c92563d
Jennychew <jennychew@jennys-MacBook-Pro.local>
2014-01-28 08:05:22 -0500

    added video

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ var switchItem = function (current, incoming) {
 	});
 };
 
+
+
 $items.filter(':not([data-state="current"])').hide();
 
 $('.next').on('click', function (){
@@ -33,3 +35,38 @@ $('.previous').on('click', function (){
 	
 	switchItem(current, next);
 });
+
+
+
+
+
+
+
+
+var $videoDialog = $('dialog');
+dialogPolyfill.registerDialog($videoDialog.get(0));
+
+$('.btn-open').on('click', function () {
+	$videoDialog.children('.video').html('<iframe src="http://player.vimeo.com/video/85066534?color=fff" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+	/*
+	When triggering built in Javascript functions we need to bypass jQuery
+	In order to get to the raw Javascript element we can use .get()
+	
+	Example:
+	$videoDialog.show(); - will trigger jQuery's show function
+		not the raw javascript show function
+	$videoDialog.get(0).show(); - will get the first raw Javascript
+		element and trigger its native show function
+	*/
+	$videoDialog.get(0).showModal();
+	/*
+	.show() allows userss to interact with stuff behind the dialog
+	.showModal() disables user interaction behind the dialog
+	*/
+});
+
+$('.btn-close').on('click', function (){
+	$videoDialog.get(0).close();
+	$videoDialog.children('.video').html('');
+});
+
