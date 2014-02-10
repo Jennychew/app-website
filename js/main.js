609172ea18342b5ed40a20db8a75656087787d26
Jennychew <jennychew@jennys-MacBook-Pro.local>
2014-02-10 09:57:42 -0500

    animation

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@ var switchItem = function (current, incoming) {
 	});
 };
 
-
-
 $items.filter(':not([data-state="current"])').hide();
 
 $('.next').on('click', function (){
@@ -39,34 +37,29 @@ $('.previous').on('click', function (){
 
 
 
-
-
-
+//video
 
 var $videoDialog = $('dialog');
 dialogPolyfill.registerDialog($videoDialog.get(0));
 
-$('.btn-open').on('click', function () {
+$('.btn-open').on('click', function (eventObject) {
+	eventObject.preventDefault();
 	$videoDialog.children('.video').html('<iframe src="http://player.vimeo.com/video/85066534?color=fff" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
-	/*
-	When triggering built in Javascript functions we need to bypass jQuery
-	In order to get to the raw Javascript element we can use .get()
-	
-	Example:
-	$videoDialog.show(); - will trigger jQuery's show function
-		not the raw javascript show function
-	$videoDialog.get(0).show(); - will get the first raw Javascript
-		element and trigger its native show function
-	*/
 	$videoDialog.get(0).showModal();
-	/*
-	.show() allows userss to interact with stuff behind the dialog
-	.showModal() disables user interaction behind the dialog
-	*/
+	
 });
 
 $('.btn-close').on('click', function (){
 	$videoDialog.get(0).close();
 	$videoDialog.children('.video').html('');
 });
+
+
+//play button
+
+var $dipper = $('.dipper');
+
+$('.dipper-section').waypoint(function (){
+	$dipper.addClass ('js-dipper-animate');
+}, {offset:'60%'});
 
